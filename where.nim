075dae17f28b2bc88a27f9cd0c1d1d4909ffd774
Samantha Demi <me@samdmarshall.com>
2019-02-18 04:09:22 -0500

    adding a new flag to disable color output

--- where.nim
+++ where.nim
@@ -1,9 +1,24 @@
 import os
 import posix
+import parseopt
 import strutils
 import terminal
 
 addQuitProc(resetAttributes)
+
+var disable_color = false
+
+var parser = initOptParser()
+for kind, key, value in parser.getopt():
+  case kind
+  of cmdLongOption, cmdShortOption:
+    case key
+    of "no-color":
+      disable_color = true
+    else:
+      discard
+  else:
+    discard
 
 var hostname = " ".repeat(255)
 let length =  gethostname(hostname, 255)
@@ -15,12 +30,16 @@ var modifier = " in "
 if existsEnv("SSH_CONNECTION"):
   modifier = " via ssh; in "
 
+
 stdout.write("on ")
-stdout.setForegroundColor(fgYellow)
+if not disable_color:
+  stdout.setForegroundColor(fgYellow)
 stdout.write(hostname)
-stdout.resetAttributes()
+if not disable_color:
+  stdout.resetAttributes()
 stdout.write(modifier)
-stdout.setForegroundColor(fgGreen)
+if not disable_color:
+  stdout.setForegroundColor(fgGreen)
 stdout.write(path)
 stdout.write("\n")
 
